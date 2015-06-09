b55243750331cddc8eba0600122753ee19287ce2
Izzat Zainol <izzatz13@gmail.com>
2015-06-09 16:43:15 +0800

    python-ssh-with-ip-range.py
    Simple example on how to utilized python module named paramiko to SSH into range of IP's system. Might be useful for system admin to do simple data collection jobs.

---
+++ python-ssh-with-ip-range.py
@@ -0,0 +1,25 @@
+import paramiko
+
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+try:
+    #specify the ip range
+    for num in range(20,30):
+        address = "192.168.1." + str(num)
+        
+        print "======================="
+        
+        ssh.connect(address, username='root', password='intel123')
+        print "Connected", address
+
+        #execute simple command on each system ip
+        stdin,stdout,stderr = ssh.exec_command("ls /root/Desktop")
+        for line in stdout.readlines():
+            print line.strip()
+
+except paramiko.SSHException:
+    print "Connection failed"
+    quit()
+
+ssh.close()
